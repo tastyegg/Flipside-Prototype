a358685683242fda8c64fddc9be8f903e7ca4693
tastyegg <garudyne@gmail.com>
2016-03-18 23:22:23 -0400

    Unity Project with integrated snap settings

---
+++ Flipside Prototype/Assets/EditModeSnapGrid.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class EditModeSnapGrid : MonoBehaviour {
+    //Changable values
+    public float snapValue = 1;
+    public float depth = 0;
+    
+	void Update ()
+    {
+        if (Application.isEditor && !Application.isPlaying)
+        {
+            float snapInverse = 1 / snapValue;
+
+            float x, y, z;
+
+            x = Mathf.Round(transform.position.x * snapInverse) / snapInverse;
+            y = Mathf.Round(transform.position.y * snapInverse) / snapInverse;
+            z = depth;
+
+            transform.position = new Vector3(x, y, z);
+        }
+    }
+}
