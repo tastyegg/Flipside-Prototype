2d281170d2b1c16e46b8a6cba81f1dbbe35fd46c
tastyegg <garudyne@gmail.com>
2016-03-20 12:59:23 -0400

    Flip with preview animation.

---
+++ Assets/FlipMechanic.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipMechanic : MonoBehaviour {
+	public static Vector2 direction;
+	public static float aniTime;
+	GameObject preview;
+
+	void Start () {
+		direction = Vector2.right;
+		aniTime = 0;
+		preview = new GameObject(transform.name + " (Preview)");
+		preview.transform.position = transform.position;
+		preview.transform.localScale = transform.localScale;
+		SpriteRenderer previewSprite = preview.AddComponent<SpriteRenderer>();
+		previewSprite.sprite = GetComponent<SpriteRenderer>().sprite;
+		previewSprite.color = Color.clear;
+	}
+	
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			SpriteRenderer previewSprite = preview.GetComponent<SpriteRenderer>();
+			previewSprite.color = Color.clear;
+			if (direction.x != 0)
+				transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+			else if (direction.y != 0)
+				transform.position = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
+		} else if (Input.GetKey(KeyCode.Space))
+		{
+			SpriteRenderer previewSprite = preview.GetComponent<SpriteRenderer>();
+			previewSprite.color = Color.blue;
+
+			if (direction.x != 0)
+			{
+				preview.transform.position = new Vector3(Mathf.Lerp(transform.position.x, - transform.position.x, aniTime), transform.position.y, transform.position.z);
+			}
+			else if (direction.y != 0)
+			{
+				preview.transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, -transform.position.y, aniTime), transform.position.z);
+			}
+		}
+	}
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -14,23 +14,35 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) ||
+			Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+			FlipMechanic.aniTime = 0.0f;
 		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
 		{
+			FlipMechanic.direction = Vector2.left;
 			playerRB.AddForce(new Vector2(-6.5f - playerRB.velocity.x, 0.0f));
 			//playerRB.velocity = new Vector2(-2.0f, playerRB.velocity.y);
 		} else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
 		{
+			FlipMechanic.direction = Vector2.right;
 			playerRB.AddForce(new Vector2( 6.5f - playerRB.velocity.x, 0.0f));
 			//playerRB.velocity = new Vector2( 2.0f, playerRB.velocity.y);
 		}
 		if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
 		{
+			FlipMechanic.aniTime = 0.0f;
+			FlipMechanic.direction = Vector2.up;
 			if (grounded)
 			{
 				grounded = false;
 				playerRB.velocity = new Vector2(playerRB.velocity.x, 6.8f);
 			}
+		} else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+		{
+			FlipMechanic.aniTime = 0.0f;
+			FlipMechanic.direction = Vector2.down;
 		}
+		FlipMechanic.aniTime += 0.11f;
 	}
 
 	void OnCollisionEnter2D(Collision2D collision)
