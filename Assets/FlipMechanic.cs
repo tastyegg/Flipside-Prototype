df554735b0c45428cb4c6628fdcde4a14d979c66
RayM4 <raymak1479@gmail.com>
2016-03-20 21:23:00 -0400

    changed flipping to Z for vertical and X for horizontal

--- Assets/FlipMechanic.cs
+++ Assets/FlipMechanic.cs
@@ -5,7 +5,7 @@ public class FlipMechanic : MonoBehaviour {
 	public static Vector2 direction;
 	public static float aniTime;
 	GameObject preview;
-    bool flipping; //used to identify middle of flip, toggle on when preview is in effect
+    bool horizontalFlip;
 
 	void Start () {
 		direction = Vector2.right;
@@ -16,7 +16,7 @@ public class FlipMechanic : MonoBehaviour {
 		SpriteRenderer previewSprite = preview.AddComponent<SpriteRenderer>();
 		previewSprite.sprite = GetComponent<SpriteRenderer>().sprite;
 		previewSprite.color = Color.clear;
-        flipping = false;
+        horizontalFlip = true;
 	}
 	
 	void Update () {
@@ -24,51 +24,32 @@ public class FlipMechanic : MonoBehaviour {
 		{
 			SpriteRenderer previewSprite = preview.GetComponent<SpriteRenderer>();
 			previewSprite.color = Color.clear;
-			if (direction.x != 0)
+			//if (direction.x != 0)
+            if (horizontalFlip)
 				transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
-			else if (direction.y != 0)
+			//else if (direction.y != 0)
+            else if (!horizontalFlip)
 				transform.position = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
 		} else if (Input.GetKey(KeyCode.Space))
 		{
 			SpriteRenderer previewSprite = preview.GetComponent<SpriteRenderer>();
 			previewSprite.color = Color.blue;
 
-			if (direction.x != 0)
+			//if (direction.x != 0)
+            if (Input.GetKey(KeyCode.X))
 			{
 				preview.transform.position = new Vector3(Mathf.Lerp(transform.position.x, - transform.position.x, aniTime), transform.position.y, transform.position.z);
 				preview.transform.eulerAngles = new Vector3(0.0f, Mathf.Lerp(0, 180, aniTime), 0.0f);
+                horizontalFlip = true;
 			}
-			else if (direction.y != 0)
+			//else if (direction.y != 0)
+            else if (Input.GetKey(KeyCode.Z))
 			{
 				preview.transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, -transform.position.y, aniTime), transform.position.z);
 				preview.transform.eulerAngles = new Vector3(Mathf.Lerp(0, 180, aniTime), 0.0f, 0.0f);
+                horizontalFlip = false;
 			}
         }
-        /*else if (Input.GetKeyUp(KeyCode.LeftAlt))
-        {
-            SpriteRenderer previewSprite = preview.GetComponent<SpriteRenderer>();
-            previewSprite.color = Color.clear;
-            if (direction.y != 0)
-                transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
-            else if (direction.x != 0)
-                transform.position = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
-        }
-        else if (Input.GetKey(KeyCode.LeftAlt))
-        {
-            SpriteRenderer previewSprite = preview.GetComponent<SpriteRenderer>();
-            previewSprite.color = Color.blue;
-
-            if (direction.y != 0)
-            {
-                preview.transform.position = new Vector3(Mathf.Lerp(transform.position.x, -transform.position.x, aniTime), transform.position.y, transform.position.z);
-                preview.transform.eulerAngles = new Vector3(0.0f, Mathf.Lerp(0, 180, aniTime), 0.0f);
-            }
-            else if (direction.x != 0)
-            {
-                preview.transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, -transform.position.y, aniTime), transform.position.z);
-                preview.transform.eulerAngles = new Vector3(Mathf.Lerp(0, 180, aniTime), 0.0f, 0.0f);
-            }
-        }*/
 	}
     
 }
