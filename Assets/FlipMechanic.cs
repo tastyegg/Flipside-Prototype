08dfe79f084dfe25dca295b08b529f27ddaabf2b
RayM4 <raymak1479@gmail.com>
2016-03-20 20:39:46 -0400

    Added vertical flip using left alt key, animation not done

--- Assets/FlipMechanic.cs
+++ Assets/FlipMechanic.cs
@@ -41,6 +41,31 @@ public class FlipMechanic : MonoBehaviour {
 				preview.transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, -transform.position.y, aniTime), transform.position.z);
 				preview.transform.eulerAngles = new Vector3(Mathf.Lerp(0, 180, aniTime), 0.0f, 0.0f);
 			}
-		}
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            SpriteRenderer previewSprite = preview.GetComponent<SpriteRenderer>();
+            previewSprite.color = Color.clear;
+            if (direction.y != 0)
+                transform.position = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            else if (direction.x != 0)
+                transform.position = new Vector3(transform.position.x, -transform.position.y, transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            SpriteRenderer previewSprite = preview.GetComponent<SpriteRenderer>();
+            previewSprite.color = Color.blue;
+
+            if (direction.y != 0)
+            {
+                preview.transform.position = new Vector3(Mathf.Lerp(transform.position.x, -transform.position.x, aniTime), transform.position.y, transform.position.z);
+                preview.transform.eulerAngles = new Vector3(0.0f, Mathf.Lerp(0, 180, aniTime), 0.0f);
+            }
+            else if (direction.x != 0)
+            {
+                preview.transform.position = new Vector3(transform.position.x, Mathf.Lerp(transform.position.y, -transform.position.y, aniTime), transform.position.z);
+                preview.transform.eulerAngles = new Vector3(Mathf.Lerp(0, 180, aniTime), 0.0f, 0.0f);
+            }
+        }
 	}
 }
