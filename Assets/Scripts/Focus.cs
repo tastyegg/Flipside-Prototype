14081831b6ce5073936baccd45d7a2405033918e
RayM4 <raymak1479@gmail.com>
2016-04-20 00:49:23 -0400

    added incrementing timer instead of instant reset, tweak increase and rateOfDecay from focus.cs to modify the rate of increase and decrease

--- Assets/Scripts/Focus.cs
+++ Assets/Scripts/Focus.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class Focus : MonoBehaviour {
     public static float FOCUS_TIMER = 10.0f;
     public float rateOfDecay = 1.0f;
+    public float increase = 3.0f;
 
     public Text textBox;
 
@@ -16,6 +17,7 @@ public class Focus : MonoBehaviour {
         focusTimer = FOCUS_TIMER;
         textBox.text = "text";
         textBox.color = Color.green;
+        dropFocus = false;
 	}
 	
 	// Update is called once per frame
@@ -24,13 +26,10 @@ public class Focus : MonoBehaviour {
 
 	    if (dropFocus) {
             focusTimer -= rateOfDecay;
+        } else if (focusTimer < FOCUS_TIMER && !dropFocus) {
+            focusTimer = Mathf.Clamp(focusTimer + (rateOfDecay / increase), 0.0f, FOCUS_TIMER);
         }
 
-        //remove
-        //if (focusTimer < 0)
-        //{
-        //    resetFocus();
-        //}
 	}
 
     public void startFocus()
@@ -42,7 +41,7 @@ public class Focus : MonoBehaviour {
     {
         //end focus mode
         dropFocus = false;
-        focusTimer = FOCUS_TIMER;
+        //focusTimer = FOCUS_TIMER;
     }
 
     public float getFocus() { return focusTimer; }
