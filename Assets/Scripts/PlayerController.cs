e87d71dc581ff2f0cf893f792094b470f4e269ee
Scott <scottlee27@hotmail.com>
2016-04-27 16:21:54 -0400

    rlev3:

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -317,7 +317,7 @@ public class PlayerController : MonoBehaviour {
             audioPlayer.PlayOneShot(smokeAudio);
             GetComponent<ParticleSystem>().Emit(500);
 			GetComponentsInChildren<ParticleSystem>()[2].gameObject.SetActive(false);
-			//GetComponentsInChildren<ParticleSystem>()[3].gameObject.SetActive(false);
+			GetComponentsInChildren<ParticleSystem>()[2].gameObject.SetActive(false);
 			Invoke("LoadNextLevel", 2.1f);
             double tRank = GameObject.Find("StarBox").GetComponent<Timer>().stop();
         }
@@ -330,11 +330,6 @@ public class PlayerController : MonoBehaviour {
             GameObject.Find("StarBox").GetComponent<Timer>().bonusStar();
             Destroy(collider.gameObject);
         }
-        /*else if (collider.gameObject.CompareTag("Portal"))
-        {
-            //run teleport
-        }*/
-        
 	}
 
 
