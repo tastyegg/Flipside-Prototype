e48ce20dc3b0406f595d210f08a1809ac02dc996
Kevin Zeng <garudyne@gmail.com>
2016-04-29 00:22:22 -0400

    Funky background spinning.

---
+++ Assets/RotateBackground.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBackground : MonoBehaviour {
+	float spinVelocity = Mathf.PI / 2;
+
+	// Update is called once per frame
+	void Update () {
+		spinVelocity += 0.0003f;
+
+		Vector3 r = transform.localEulerAngles;
+		r.z += Mathf.Sin(spinVelocity) * 0.04f * Time.timeScale;
+		transform.localEulerAngles = r;
+	}
+}
