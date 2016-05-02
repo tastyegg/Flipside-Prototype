db445d291058f67c4d22b2e2aa86ed7e74d41d6d
RayM4 <raymak1479@gmail.com>
2016-05-02 03:36:29 -0400

    Created functional Title Screen, just need to style it and figure out what levels we need

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+    public void LoadScene(string level)
+    {
+        //Application.LoadLevel(level);
+        SceneManager.LoadScene(level);
+    }
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
