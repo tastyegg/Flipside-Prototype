a8d187ea7021c902c93a0e03f88b63c6d60875ff
Scott <scottlee27@hotmail.com>
2016-05-04 12:45:11 -0400

    camera changes

--- Assets/SetPrefab.cs
+++ Assets/SetPrefab.cs
@@ -7,7 +7,7 @@ public class SetPrefab : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Vector3 pos = transform.position;
-		pos.z = -50;
+		pos.z = -45;
 		transform.position = pos;
 	}
 	
